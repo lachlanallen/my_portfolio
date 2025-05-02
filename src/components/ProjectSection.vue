7a53f4dacff6e496e367ea4c4a276df3dad75e7a
Lachlan Allen <lallen72@uncc.edu>
2025-05-02 16:21:20 -0400

    fixed project image imports

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,10 +1,17 @@
 <script setup>
 import { ref, computed } from 'vue';
+
+import lithavenImg from '@/assets/project-imgs/lithaven-thumbnail.png';
+import climacastImg from '@/assets/project-imgs/climacast-thumbnail.png';
+import sproutGrowImg from '@/assets/project-imgs/sprout-grow-thumbnail.png';
+import pokesearchImg from '@/assets/project-imgs/pokesearch-thumbnail.png';
+import alpineImg from '@/assets/project-imgs/alpine-thumbnail.png';
+
 const Projects = ref([
     {
         id: 1,
         category: 'prototypes',
-        image: 'src/assets/project-imgs/lithaven-thumbnail.png',
+        image: lithavenImg, 
         title: 'Lithaven',
         description: 'Lithaven is an app designed to support readers, of all ages, in their journey to find and read new books.',
         technologies: ['Figma'],
@@ -14,7 +21,7 @@ const Projects = ref([
     {
         id: 2,
         category: 'websites',
-        image: 'src/assets/project-imgs/climacast-thumbnail.png',
+        image: climacastImg, 
         title: 'Climacast',
         description: 'An intuitive weather application designed to provide users with real-time forecasts and dynamic weather visualizations.',
         technologies: ['HTML', 'CSS', 'JavaScript', 'Vue.js'],
@@ -24,7 +31,7 @@ const Projects = ref([
     {
         id: 3,
         category: 'websites',
-        image: 'src/assets/project-imgs/sprout-grow-thumbnail.png',
+        image: sproutGrowImg, 
         title: 'Sprout & Grow',
         description: 'A dynamic website for Sprout & Grow, a fictional client passionate about sustainable gardening.',
         technologies: ['HTML', 'CSS'],
@@ -34,7 +41,7 @@ const Projects = ref([
     {
         id: 4,
         category: 'websites',
-        image: 'src/assets/project-imgs/pokesearch-thumbnail.png',
+        image: pokesearchImg, 
         title: 'PokeSearch',
         description: 'A web app that utilizes PokeAPI to display Generation 7 Pokemon and their information.',
         technologies: ['HTML', 'Tailwind CSS', 'JavaScript', 'React.js'],
@@ -44,7 +51,7 @@ const Projects = ref([
     {
         id: 5,
         category: 'prototypes',
-        image: 'src/assets/project-imgs/alpine-thumbnail.png',
+        image: alpineImg, 
         title: 'Alpine Serenity Inn',
         description: 'A bed and breakfast prototype designed to offer users a seamless experience in booking their stay.',
         technologies: ['Figma'],
